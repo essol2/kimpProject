c2020e166fc4ebb7d9cfbed25c36d41b278318e0
Eunsol Lee <73922496+essol2@users.noreply.github.com>
2021-09-27 01:32:01 +0900

    simpleCommit

---
+++ kimpProject/src/main/java/com/kita/kimp/KimpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kita.kimp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimpProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimpProjectApplication.class, args);
+	}
+
+}
---
+++ kimpProject/src/test/java/com/kita/kimp/KimpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kita.kimp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimpProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
